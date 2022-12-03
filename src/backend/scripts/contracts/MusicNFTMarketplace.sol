9fc85f8dc78c6ceb81f996a00bd06ab0e1b33e7d
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-12-03 10:26:22 +0000

    smart contract updates

---
+++ src/backend/scripts/contracts/MusicNFTMarketplace.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+ 
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MusicNFTMarketplace is ERC721("DAppFi", "DAPP"), Ownable {
+     string public baseURI =
+        "https://bafybeidhjjbjonyqcahuzlpt7sznmh4xrlbspa3gstop5o47l6gsiaffee.ipfs.nftstorage.link/";
+        
+}
