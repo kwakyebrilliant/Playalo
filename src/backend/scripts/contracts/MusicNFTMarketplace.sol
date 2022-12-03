b6ddc8fab91982de881ec7bce2e14672ecc4627c
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-12-03 10:04:51 +0000

    backend updates

--- src/backend/scripts/contracts/MusicNFTMarketplace.sol
+++ src/backend/scripts/contracts/MusicNFTMarketplace.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract MusicNFTMarketplace is ERC721, Ownable {
+contract MusicNFTMarketplace is ERC721("DAppFi", "DAPP"), Ownable {
      string public baseURI =
         "https://bafybeidhjjbjonyqcahuzlpt7sznmh4xrlbspa3gstop5o47l6gsiaffee.ipfs.nftstorage.link/";
         
--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -7,6 +7,9 @@ async function main() {
 
   // deploy contracts here:
   const NFTMarketplaceFactory = await ethers.getContractFactory("MusicNFTMarketplace");
+  const nftMarketplace = await NFTMarketplaceFactory.deploy()
+
+  console.log("Smart contract address:", nftMarketplace.address)
   
   // For each contract, pass the deployed contract and name to this function to save a copy of the contract ABI and address to the front end.
   saveFrontendFiles();
