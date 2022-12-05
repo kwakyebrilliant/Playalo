7927779255260c8fa6a1db0cbf46905da4104155
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-12-05 11:09:02 +0000

    smart contract updates

--- src/backend/scripts/contracts/MusicNFTMarketplace.sol
+++ src/backend/scripts/contracts/MusicNFTMarketplace.sol
@@ -18,6 +18,23 @@ contract MusicNFTMarketplace is ERC721("DAppFi", "DAPP"), Ownable {
         }
         MarketItem[] public marketItems;
 
-        
+        constructor(
+                uint256 _royaltyFee,
+                address _artist,
+                uint256[] memory _prices
+        ) payable {
+                require(
+                _prices.length * _royaltyFee <= msg.value,
+                "Deployer must pay royalty fee for each token listed on the marketplace"
+                );
+                
+                royaltyFee = _royaltyFee;
+                artist = _artist;
+                for (uint8 i = 0; i < _prices.length; i++) {
+                require(_prices[i] > 0, "Price must be greater than 0");
+                _mint(address(this), i);
+                marketItems.push(MarketItem(i, payable(msg.sender), _prices[i]));
+                }
+        }
         
 }
