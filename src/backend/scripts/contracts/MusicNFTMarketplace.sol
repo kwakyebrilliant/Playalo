2e4557435fd0391fca5dea93fde71e1c5f71d694
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-12-05 10:31:54 +0000

    smart contract updates

--- src/backend/scripts/contracts/MusicNFTMarketplace.sol
+++ src/backend/scripts/contracts/MusicNFTMarketplace.sol
@@ -5,7 +5,10 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MusicNFTMarketplace is ERC721("DAppFi", "DAPP"), Ownable {
-     string public baseURI =
-        "https://bafybeidhjjbjonyqcahuzlpt7sznmh4xrlbspa3gstop5o47l6gsiaffee.ipfs.nftstorage.link/";
+        string public baseURI =
+                "https://bafybeidhjjbjonyqcahuzlpt7sznmh4xrlbspa3gstop5o47l6gsiaffee.ipfs.nftstorage.link/";
+        string public baseExtension = ".json";
+        address public artist;
+        uint256 public royaltyFee;
         
 }
