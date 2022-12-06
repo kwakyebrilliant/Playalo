a746204c2634952e3c42222de2a2dcf54c86255b
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-12-06 11:28:58 +0000

    test updates

---
+++ src/backend/test/MusicNFTMarketplace.test.js
@@ -0,0 +1,45 @@
+const { expect } = require("chai");
+
+const toWei = (num) => ethers.utils.parseEther(num.toString())
+const fromWei = (num) => ethers.utils.formatEther(num)
+
+describe("MusicNFTMarketplace", function () {
+
+    let nftMarketplace
+    let deployer, artist, user1, user2, users;
+    let royaltyFee = toWei(0.01); // 1 ether = 10^18 wei
+    let URI = "https://bafybeidhjjbjonyqcahuzlpt7sznmh4xrlbspa3gstop5o47l6gsiaffee.ipfs.nftstorage.link/"
+    let prices = [toWei(1), toWei(2), toWei(3), toWei(4), toWei(5), toWei(6), toWei(7), toWei(8)]
+    let deploymentFees = toWei(prices.length * 0.01)
+
+
+    beforeEach(async function () {
+        // Get the ContractFactory and Signers here.
+        const NFTMarketplaceFactory = await ethers.getContractFactory("MusicNFTMarketplace");
+        [deployer, artist, user1, user2, ...users] = await ethers.getSigners();
+    
+        // Deploy music nft marketplace contract 
+        nftMarketplace = await NFTMarketplaceFactory.deploy(
+          royaltyFee,
+          artist.address,
+          prices,
+          { value: deploymentFees }
+        );
+    
+      });
+
+      describe("Deployment", function () {
+
+        it("Should track name, symbol, URI, royalty fee and artist", async function () {
+          const nftName = "DAppFi"
+          const nftSymbol = "DAPP"
+          expect(await nftMarketplace.name()).to.equal(nftName);
+          expect(await nftMarketplace.symbol()).to.equal(nftSymbol);
+          expect(await nftMarketplace.baseURI()).to.equal(URI);
+          expect(await nftMarketplace.royaltyFee()).to.equal(royaltyFee);
+          expect(await nftMarketplace.artist()).to.equal(artist.address);
+        });
+
+    });
+
+});
