2580e1a762ff8a6798caf5db79e5345fe34e7714
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-12-12 14:35:47 +0000

    app updates

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -10,46 +10,26 @@ import logo from './logo.png';
 import './App.css';
  
 function App() {
+  const [loading, setLoading] = useState(true)
+  const [account, setAccount] = useState(null)
+
   return (
-    <div>
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a
-          className="navbar-brand col-sm-3 col-md-2 ms-3"
-          href="http://www.dappuniversity.com/bootcamp"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Playalo
-        </a>
-      </nav>
-      <div className="container-fluid mt-5">
-        <div className="row">
-          <main role="main" className="col-lg-12 d-flex text-center">
-            <div className="content mx-auto mt-5">
-              <a
-                href="http://www.dappuniversity.com/bootcamp"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={logo} className="App-logo" alt="logo"/>
-              </a>
-              <h1 className= "mt-5">Playalo</h1>
-              <p>
-                Edit <code>src/frontend/components/App.js</code> and save to reload.
-              </p>
-              <a
-                className="App-link"
-                href="http://www.dappuniversity.com/bootcamp"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-              </a>
-            </div>
-          </main>
-        </div>
+     <BrowserRouter>
+      <div className="App">
+        <>
+          <Navbar expand="lg" bg="secondary" variant="dark">
+            <Container>
+              <Navbar.Brand href="http://www.dappuniversity.com/bootcamp">
+                <img src={logo} width="40" height="40" className="" alt="" />
+                &nbsp; Playalo
+              </Navbar.Brand>
+
+            </Container>
+          </Navbar>
+        </>
       </div>
-    </div>
+    </BrowserRouter>
+
   );
 }
 
