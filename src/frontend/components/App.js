9ef2a1aebc12fcc0a82dfb649e4e1b2da200ecce
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-12-12 14:42:21 +0000

    app updates

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -23,10 +23,49 @@ function App() {
                 <img src={logo} width="40" height="40" className="" alt="" />
                 &nbsp; Playalo
               </Navbar.Brand>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link as={Link} to="/">Home</Nav.Link>
+                  <Nav.Link as={Link} to="/my-tokens">My Tokens</Nav.Link>
+                  <Nav.Link as={Link} to="/my-resales">My Resales</Nav.Link>
+                </Nav>
+                <Nav>
+                  {account ? (
+                    <Nav.Link
+                      href={`https://etherscan.io/address/${account}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="button nav-button btn-sm mx-4">
+                      <Button variant="outline-light">
+                        {account.slice(0, 5) + '...' + account.slice(38, 42)}
+                      </Button>
 
+                    </Nav.Link>
+                  ) : (
+                    <Button variant="outline-light">Connect Wallet</Button>
+                  )}
+                </Nav>
+              </Navbar.Collapse>
             </Container>
           </Navbar>
         </>
+
+        <div>
+          {loading ? (
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '80vh' }}>
+              <Spinner animation="border" style={{ display: 'flex' }} />
+              <p className='mx-3 my-0'>Awaiting Metamask Connection...</p>
+            </div>
+          ) : (
+            <Routes>
+              <Route path="/" />
+              <Route path="/my-tokens" />
+              <Route path="/my-resales" />
+            </Routes>
+          )}
+        </div>
+
       </div>
     </BrowserRouter>
 
