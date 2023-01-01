7486c4aad9d5a5f32c34b5b91d998229aa8d3dd8
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-01-01 11:03:20 +0000

    app updates

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -80,7 +80,7 @@ function App() {
               <Route path="/my-tokens" />
               <Route path="/my-resales" />
             </Routes>
-          )}
+          )} 
         </div>
 
       </div>
