bc9ba4c174f93f16c2c06a13c612a8c6d00dd511
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-01-06 16:48:34 +0000

    home updates

--- src/frontend/components/Home.js
+++ src/frontend/components/Home.js
@@ -85,6 +85,32 @@ const Home = ({ contract }) => {
                                 <Card.Img variant="top" src={marketItems[currentItemIndex].identicon} />
                                     <Card.Body color="secondary">
                                         <Card.Title as="h2" > {marketItems[currentItemIndex].name}</Card.Title>
+                                        
+                                            <div className="d-grid px-4">
+                                                <ButtonGroup size="lg" aria-label="Basic example">
+                                                <Button variant="secondary" onClick={() => skipSong(false)}>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-skip-backward" viewBox="0 0 16 16">
+                                                    <path d="M.5 3.5A.5.5 0 0 1 1 4v3.248l6.267-3.636c.52-.302 1.233.043 1.233.696v2.94l6.267-3.636c.52-.302 1.233.043 1.233.696v7.384c0 .653-.713.998-1.233.696L8.5 8.752v2.94c0 .653-.713.998-1.233.696L1 8.752V12a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5zm7 1.133L1.696 8 7.5 11.367V4.633zm7.5 0L9.196 8 15 11.367V4.633z" />
+                                                    </svg>
+                                                </Button>
+                                                <Button variant="secondary" onClick={() => setIsPlaying(!isPlaying)}>
+                                                    {isPlaying ? (
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-pause" viewBox="0 0 16 16">
+                                                        <path d="M6 3.5a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5zm4 0a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5z" />
+                                                    </svg>
+                                                    ) : (
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-play" viewBox="0 0 16 16">
+                                                        <path d="M10.804 8 5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z" />
+                                                    </svg>
+                                                    )}
+                                                </Button>
+                                                <Button variant="secondary" onClick={() => skipSong(true)}>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-skip-forward" viewBox="0 0 16 16">
+                                                    <path d="M15.5 3.5a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V8.752l-6.267 3.636c-.52.302-1.233-.043-1.233-.696v-2.94l-6.267 3.636C.713 12.69 0 12.345 0 11.692V4.308c0-.653.713-.998 1.233-.696L7.5 7.248v-2.94c0-.653.713-.998 1.233-.696L15 7.248V4a.5.5 0 0 1 .5-.5zM1 4.633v6.734L6.804 8 1 4.633zm7.5 0v6.734L14.304 8 8.5 4.633z" />
+                                                    </svg>
+                                                </Button>
+                                                </ButtonGroup>
+                                            </div>
                             
                             </Card.Body>
                             <Card.Footer>
